931ff2636d738785bf8e6b5478c483d844f42a9e
BogdanShulga <bogdan.shulga.90@gmail.com>
2019-09-14 13:46:47 +0300

    master: initial commit

---
+++ src/main/java/com/edu/movies_rating_review_platform/MoviesRatingReviewPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.edu.movies_rating_review_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesRatingReviewPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesRatingReviewPlatformApplication.class, args);
+    }
+
+}
